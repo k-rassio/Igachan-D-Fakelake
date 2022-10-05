b3f7ec5225b4fb6224f1bbfaba5afc4edecda563
tatsuki.kurihara <t.kurihara@tech.nflabs.jp>
2022-10-06 08:10:39 +0900

    全ての項目を追加

--- app/hello.py
+++ app/hello.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/top')
 def top():
-    category = ['社会​', '気象・災害​', '科学・文化​', '政治​', 'ビジネス​', 'スポーツ​', '暮らし​', '医療・健康​']
-    csv_name = ['1.csv', '2.csv', '3.csv', '4.csv', '5.csv', '6.csv', '7.csv', '8.csv']
+    category = ['社会​', '気象・災害​', '科学・文化​', '政治​', 'ビジネス​', 'スポーツ​', '暮らし​', '医療・健康​', '全て']
+    csv_name = ['Dataset_society.csv', 'Dataset_weather and disaster.csv', 'Dataset_science and culture.csv', 'Dataset_politics.csv', 'Dataset_buisiness.csv', 'Dataset_sports.csv', 'Dataset_living.csv', 'Dataset_health.csv', 'Dataset_all.csv']
     cat_url = dict(zip(category, csv_name))
     return render_template('top.html', cat_url=cat_url)
 
@@ -15,7 +15,7 @@ def top():
 def download():
     csv_name = request.args.get('fname_ext').split(',')[0]
     extention = request.args.get('fname_ext').split(',')[1]
-    csv_path = './data/' + csv_name
+    csv_path = './data/csv/' + csv_name
     if extention == 'csv':
         return send_file(csv_path, as_attachment=True, download_name=csv_name, mimetype='text/csv')
     elif extention == 'json':
@@ -26,7 +26,7 @@ def download():
 
         # JSONファイルへの書き込み
         json_name = csv_name.replace('csv', 'json')
-        json_path = './data/' + json_name
+        json_path = './data/json/' + json_name
         with open(file=json_path, mode='w', encoding="utf-8") as f:
             json.dump(d_list, f, ensure_ascii=False)
         return send_file(json_path, as_attachment=True, download_name=json_name, mimetype='application/json')
@@ -39,7 +39,7 @@ def detail():
     return render_template("detail.html", data=data) # templatesフォルダ内のindex.htmlを表示する
 
 def read_csv(file_name):
-    with open('./data/' + file_name, mode='r', encoding="utf-8") as f:
+    with open('./data/csv/' + file_name, mode='r', encoding="utf-8") as f:
         data = list(csv.reader(f))
     return data
 
